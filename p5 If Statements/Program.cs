5b75f11bfff115169c66c1770517068b5e154607
Cash Sullivan <Cashman7820@gmail.com>
2023-03-08 10:48:12 -0500

    First program can no longer crash

--- p5 If Statements/Program.cs
+++ p5 If Statements/Program.cs
@@ -4,45 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Double Balance, fee, StartingBal, DepoAmt, WitDra ;
-            int option1, option2, option3, option;
+            Double Balance, fee, StartingBal, Deposit, WithDraw ;
+            int option;
+            bool done = false;
+
             Random Generator = new Random();
             StartingBal = Generator.Next(100, 151);
 
-            Console.WriteLine("Hello User!, Welcome to B.O.B");
-            Console.WriteLine("What Option would you like to do?");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("1: DEPOSIT");
-            Console.WriteLine("2: WITHDRAW");
-            Console.WriteLine("3: QUIT");
-            Console.WriteLine("---------------------------------");
-            int program;
-            if (Int32.TryParse(Console.ReadLine(), out option))
+            Console.WriteLine("Welcome to B.O.B");
+
+            while (done != true)
             {
-                if (option == 1)
+                Console.WriteLine("What Option would you like to do?");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("1: DEPOSIT");
+                Console.WriteLine("2: WITHDRAW");
+                Console.WriteLine("3: QUIT");
+                Console.WriteLine("---------------------------------");
+                if (int.TryParse(Console.ReadLine(), out option))
                 {
-                    option1();
-                }
-                else if (option == 2)
-                {
-                    option2();
-                }
-                else if (option == 3)
-                {
-                    option3();
+                    if (option == 1)
+                    {
+                        Console.WriteLine("TEST 1");
+                    }
+                    else if (option == 2)
+                    {
+                        Console.WriteLine("TEST 2");
+                    }
+                    else if (option == 3)
+                    {
+                        Console.WriteLine("TEST 3");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("That isn't an option!");
+                    Console.WriteLine("INVALID INPUT! try again.");
+
                 }
             }
-            else
-            {
-                Console.WriteLine("Invalid Input.");
-            }
-
-
-
         }
     }
 }
