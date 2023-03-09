8f372eabf23fd94095b0cb09753cdecce1941b39
Cash Sullivan <Cashman7820@gmail.com>
2023-03-09 12:00:05 -0500

    Added Both Deposit and Withdrawal

--- p5 If Statements/Program.cs
+++ p5 If Statements/Program.cs
@@ -1,16 +1,19 @@
-﻿namespace p5_If_Statements
+﻿using System.Reflection.Metadata;
+using System.Transactions;
+
+namespace p5_If_Statements
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Double Balance, fee, StartingBal, Deposit, WithDraw ;
+            Double Balance, fee, StartingBal, Deposit, WithDraw;
             int option;
             bool done = false;
-
+            fee = 0.75;
             Random Generator = new Random();
             StartingBal = Generator.Next(100, 151);
-
+            Balance = StartingBal;
             Console.WriteLine("Welcome to B.O.B");
 
             while (done != true)
@@ -18,28 +21,72 @@
                 Console.WriteLine("What Option would you like to do?");
                 Console.WriteLine("---------------------------------");
                 Console.WriteLine("1: DEPOSIT");
-                Console.WriteLine("2: WITHDRAW");
-                Console.WriteLine("3: QUIT");
+                Console.WriteLine("2: WITHDRAWAL");
+                Console.WriteLine("3: BILL PAYMENT");
+                Console.WriteLine("4: BALANCE UPDATE");
+                Console.WriteLine("5: QUIT");
                 Console.WriteLine("---------------------------------");
-                if (int.TryParse(Console.ReadLine(), out option)&& option >= 1 && option <= 3)
+                if (int.TryParse(Console.ReadLine(), out option)&& option >= 1 && option <= 5)
                 {
                     if (option == 1)
                     {
-                        Console.WriteLine("TEST 1");
-                        Thread.Sleep(1000);
-                        Console.WriteLine();
+                        Console.WriteLine("You currently have " + Balance + "$");
+                        Console.WriteLine("What would you like to deposit?(0.75$ fee)");
+                        if (Double.TryParse(Console.ReadLine(), out Deposit))
+                        {
+                            if (Deposit < 5)
+                                Console.WriteLine("Transaction Failed");
+                            else
+                            {
+                                Balance = Balance + Deposit - fee;
+                                Balance = Math.Round(Balance, 2);
+                                Console.WriteLine();
+                                Console.WriteLine("Account Update: " + Balance + "$");
+                            }
+                        }
+                        else
+                            Console.WriteLine("Invalid Input");
                     }
                     else if (option == 2)
                     {
-                        Console.WriteLine("TEST 2");
-                        Thread.Sleep(1000);
-                        Console.WriteLine();
+                        Console.WriteLine("You currently have " + Balance + "$");
+                        Console.WriteLine("How much would you like to withdraw?(0.75$ fee)");
+
+                        if (Double.TryParse(Console.ReadLine(), out WithDraw))
+                        {
+                            if (WithDraw > Balance - fee)
+                            {
+                                Console.WriteLine("Invaild Input");
+                            }
+                        }
+                        else if (WithDraw == Balance - fee)
+                        {
+                            Console.WriteLine("Invaild Input");
+                        }
+                        else
+                        {
+                           
+                            Balance = Balance - WithDraw - fee;
+                            Console.WriteLine($"Your new Balance is {Balance}");
+                            WithDraw = Math.Round(WithDraw, 2);
+                        }
+
+
                     }
                     else if (option == 3)
                     {
-                        Console.WriteLine("TEST 3");
-                        Thread.Sleep(1000);
+
+                    }
+                    else if (option == 4)
+                    {
+                        
+                    }
+                    else if (option == 5)
+                    {
                         Console.WriteLine();
+                        Console.WriteLine("Have a good day!");
+                        Thread.Sleep(3000);
+                        done = true;
                     }
                 }
                 else
