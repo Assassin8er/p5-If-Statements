2af3d2842f29a17e3844dff8ecaf965c83851f58
Cash Sullivan <Cashman7820@gmail.com>
2023-03-20 11:03:42 -0400

    Finished Code For Bank of Blorb

--- p5 If Statements/Program.cs
+++ p5 If Statements/Program.cs
@@ -7,17 +7,23 @@ namespace p5_If_Statements
     {
         static void Main(string[] args)
         {
-            Double Balance, fee, StartingBal, Deposit, WithDraw;
+            Double Balance, fee, StartingBal, Deposit, WithDraw, Bill, Interest;
             int option;
+            string choice;
             bool done = false;
             fee = 0.75;
+            Random Generator1 = new Random();
+            Bill = Generator1.Next(100, 151);
             Random Generator = new Random();
             StartingBal = Generator.Next(100, 151);
             Balance = StartingBal;
+            Interest = Bill / 10;
+            Math.Round(Bill, 2);
             Console.WriteLine("Welcome to B.O.B");
 
             while (done != true)
             {
+                Console.WriteLine($"Your Balance is:{Balance}$");
                 Console.WriteLine("What Option would you like to do?");
                 Console.WriteLine("---------------------------------");
                 Console.WriteLine("1: DEPOSIT");
@@ -26,11 +32,11 @@ namespace p5_If_Statements
                 Console.WriteLine("4: BALANCE UPDATE");
                 Console.WriteLine("5: QUIT");
                 Console.WriteLine("---------------------------------");
-                if (int.TryParse(Console.ReadLine(), out option)&& option >= 1 && option <= 5)
+                if (int.TryParse(Console.ReadLine(), out option) && option >= 1 && option <= 5)
                 {
                     if (option == 1)
                     {
-                        Console.WriteLine("You currently have " + Balance + "$");
+                        Console.WriteLine($"You currently have {Balance}$.");
                         Console.WriteLine("What would you like to deposit?(0.75$ fee)");
                         if (Double.TryParse(Console.ReadLine(), out Deposit))
                         {
@@ -41,7 +47,7 @@ namespace p5_If_Statements
                                 Balance = Balance + Deposit - fee;
                                 Balance = Math.Round(Balance, 2);
                                 Console.WriteLine();
-                                Console.WriteLine("Account Update: " + Balance + "$");
+                                Console.WriteLine($"Account Update: {Balance}$");
                             }
                         }
                         else
@@ -49,7 +55,7 @@ namespace p5_If_Statements
                     }
                     else if (option == 2)
                     {
-                        Console.WriteLine("You currently have " + Balance + "$");
+                        Console.WriteLine($"You currently have {Balance} $.");
                         Console.WriteLine("How much would you like to withdraw?(0.75$ fee)");
 
                         if (Double.TryParse(Console.ReadLine(), out WithDraw))
@@ -58,28 +64,52 @@ namespace p5_If_Statements
                             {
                                 Console.WriteLine("Invaild Input");
                             }
-                        }
-                        else if (WithDraw == Balance - fee)
-                        {
-                            Console.WriteLine("Invaild Input");
+
+                            else if (WithDraw == Balance - fee && 0 > Balance)
+                            {
+                                Console.WriteLine("Invaild Input");
+                            }
+                            else
+                            {
+
+                                Balance = Balance - WithDraw - fee;
+                                WithDraw = Math.Round(WithDraw, 2);
+                                Console.WriteLine($"Your new Balance is {Balance}$.");
+                            }
                         }
                         else
-                        {
-                           
-                            Balance = Balance - WithDraw - fee;
-                            Console.WriteLine($"Your new Balance is {Balance}");
-                            WithDraw = Math.Round(WithDraw, 2);
-                        }
-
-
+                            Console.WriteLine("Invalid Input");
                     }
                     else if (option == 3)
                     {
+                        Console.WriteLine($"Bills Payable: 1. Amount owed: {Bill}$");
+                        Console.WriteLine("Would you like to pay this bill? Yes, No?");
+                        choice = Console.ReadLine().ToLower();
+                        if (choice == "yes" || choice == "yes " || choice == " yes" || choice == "yes." || choice == "y")
+                        {
+                            if (Bill > Balance)
+                                Console.WriteLine("Invalid Funds.");
+                            else
+                            {
+                                Console.WriteLine("Transaction Complete!");
+                                Balance = Balance - Bill;
+                                Console.WriteLine($"Your new Balance is {Balance}$.");
+                            }
 
+                        }
+                        else if (choice == "no" || choice == "no " || choice == " no" || choice == "no." || choice == "n")
+                        {
+                            Console.WriteLine("10% interest added to bill...");
+                            Bill = Bill + Interest;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input! Try Again.");
+                        }
                     }
                     else if (option == 4)
                     {
-                        
+                        Console.WriteLine($"Your new balance is {Balance}$");
                     }
                     else if (option == 5)
                     {
@@ -92,7 +122,7 @@ namespace p5_If_Statements
                 else
                 {
                     Console.WriteLine();
-                    Console.WriteLine("INVALID INPUT!!! Try putting in \"1\", \"2\", \"3\".");
+                    Console.WriteLine("INVALID INPUT!!! Try putting in \"1\", \"2\", \"3\", \"4\", \"5\".");
                     Thread.Sleep(1000);
                     Console.WriteLine();
                 }
